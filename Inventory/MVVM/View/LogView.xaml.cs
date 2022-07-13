79bcb8c06a3ecaf250bcf98c51e9f98088d363bd
Frans Sadicon <fassadicon@iskolarngbayan.pup.edu.ph>
2022-07-13 19:36:04 +0800

    Log Date v1

--- Inventory/MVVM/View/LogView.xaml.cs
+++ Inventory/MVVM/View/LogView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,40 @@ namespace Inventory.MVVM.View
         public LogView()
         {
             InitializeComponent();
+            LoadGrid();
+            StartDate.SelectedDate = DateTime.Today;
+            EndDate.SelectedDate = DateTime.Today;
+        }
+        // INVENTORY DATABASE T SHIRT DETAILS TABLE
+        public void LoadGrid()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\FAsad\\source\\repos\\NewRepo\\Inventory\\InventoryDB.mdf;Integrated Security=True"))
+                {
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM DeliveryDetails WHERE DateReceived = '"+ StartDate.Text+ "' OR DateDelivered = '" + StartDate.Text + "' OR DateReceived = '" + EndDate.Text + "' OR DateDelivered = '" + EndDate.Text + "';", conn);
+                    DataTable dt = new DataTable();
+                    conn.Open();
+                    SqlDataReader sdr = cmd.ExecuteReader();
+                    dt.Load(sdr);
+                    conn.Close();
+                    Deliveries.ItemsSource = dt.DefaultView;
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Reading Failed: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void StartDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadGrid();
+        }
+
+        private void EndDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadGrid();
         }
     }
 }
